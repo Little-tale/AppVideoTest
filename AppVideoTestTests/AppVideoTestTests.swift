e06baa237b9fd33d24dd50f33a2dcc1705ac40b1
Kim JaeHyung <you3192001@icloud.com>
2024-10-28 19:52:59 +0900

    Initial Commit

---
+++ AppVideoTestTests/AppVideoTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppVideoTestTests.swift
+//  AppVideoTestTests
+//
+//  Created by Jae hyung Kim on 10/28/24.
+//
+
+import Testing
+@testable import AppVideoTest
+
+struct AppVideoTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
