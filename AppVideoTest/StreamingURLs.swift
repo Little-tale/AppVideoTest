6535a17375cb192c00baba0c05b11b77588bdad8
Kim JaeHyung <you3192001@icloud.com>
2024-11-18 16:15:51 +0900

    [UPDATE] 스트리밍 URL 추가

--- AppVideoTest/StreamingURLs.swift
+++ AppVideoTest/StreamingURLs.swift
@@ -11,6 +11,9 @@ enum StreamingURLs {
     static let urls: [String] = [
         "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8",
         "https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8",
+        "https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8",
+        "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.mp4/.m3u8",
+        "https://ireplay.tv/test/blender.m3u8",
     ]
     
     static func getRandomURLString() -> String {
